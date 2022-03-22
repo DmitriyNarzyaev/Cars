c911ac69213a47073d7f92814b7559ac91d4f46a
Dmitriy Narzyaev <nozaki@yandex.ru>
2022-03-22 17:22:05 +0300

    initial commit

---
+++ src/Main_Container.ts
@@ -0,0 +1,20 @@
+import Container = PIXI.Container;
+import { Graphics } from "pixi.js";
+
+export default class Main_Container extends Container {
+	public static readonly WIDTH:number = 3000;
+	public static readonly HEIGHT:number = 1500;
+
+	constructor() {
+		super();
+
+		this.initBackground();
+	}
+
+	private initBackground():void {
+		let plkdjfg: Graphics = new Graphics;
+		plkdjfg.beginFill(0x00ff48);
+		plkdjfg.drawRect(0, 0, Main_Container.WIDTH, Main_Container.HEIGHT);
+		this.addChild(plkdjfg);
+	}
+}
