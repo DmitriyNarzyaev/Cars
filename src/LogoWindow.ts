a3364fcd5736b97d8d2b24ea7f4621b2c97242a5
Dmitriy Narzyaev <nozaki@yandex.ru>
2022-03-22 19:44:14 +0300

    added logo window

---
+++ src/LogoWindow.ts
@@ -0,0 +1,62 @@
+import { Sprite } from "pixi.js";
+import Container = PIXI.Container;
+
+export default class LogoWindow extends Container {
+	private _logoSizeCorrector:number = 4;
+	private _windowWidth:number = 800;
+	private _windowHeight:number = 500 / this._logoSizeCorrector;
+	constructor(logoNameArray:string[]) {
+		super();
+		this.initBorders();
+		this.initLogo(logoNameArray);
+	}
+
+	private initBorders():void {
+		let borders:PIXI.Graphics = new PIXI.Graphics;
+		borders
+			.lineStyle(1, 0x000000, 1, 1)
+			.beginFill(0x000000, 0)
+			.drawRect(0, 0, this._windowWidth, this._windowHeight);
+		this.addChild(borders);
+	}
+
+	private initLogo(logoNameArray:string[]):void {
+		const gap:number = 10;
+		let logoBackgroundX:number = 0;
+		let logoContainer:PIXI.Container = new PIXI.Container;
+		this.addChild(logoContainer);
+
+		for (let iterator:number = 0; iterator < logoNameArray.length; iterator++) {
+			let logoBackground:PIXI.Graphics = new PIXI.Graphics;
+			logoBackground
+				.beginFill(0xaa4400)
+				.drawRect(0, 0, this._windowHeight, this._windowHeight);
+			logoBackground.x = logoBackgroundX;
+			logoBackgroundX += logoBackground.width + gap;
+			logoContainer.addChild(logoBackground);
+
+			let logo:Sprite = Sprite.from(logoNameArray[iterator]);
+			logo.width /= this._logoSizeCorrector;
+			logo.height /= this._logoSizeCorrector;
+			logo.x = logoBackground.x + (logoBackground.width - logo.width) /2;
+			logo.y = logoBackground.y + (logoBackground.height - logo.height) /2;
+			logoContainer.addChild(logo);
+		}
+
+		let logoContainerBackground:PIXI.Graphics = new PIXI.Graphics;
+		logoContainerBackground
+			.lineStyle(1, 0x000000, 0, 0)
+			.beginFill(0x000000, 1)
+			.drawRect(0, 0, logoContainer.width, logoContainer.height);
+		logoContainer.addChildAt(logoContainerBackground, 0);
+
+		let windowMask:PIXI.Graphics = new PIXI.Graphics;
+		windowMask
+			.lineStyle(1, 0x000000, 0, 0)
+			.beginFill(0x000000, 1)
+			.drawRect(0, 0, this._windowWidth, this._windowHeight);
+		this.addChild(windowMask);
+
+		logoContainer.mask = windowMask;
+	}
+}
--- src/MainContainer.ts
+++ src/MainContainer.ts
@@ -1,18 +1,23 @@
 import Container = PIXI.Container;
-import { Graphics, Loader } from "pixi.js";
+import { Graphics, Loader,  } from "pixi.js";
+import LogoWindow from "./LogoWindow";
 
 export default class MainContainer extends Container {
 	public static readonly WIDTH:number = 1300;
 	public static readonly HEIGHT:number = 600;
 	private _pictureNameArray:string[] = [
-		"AlfaRomeo",		"AlfaRomeoLogo",
-		"AstonMartin",		"AstonMartinLogo",
-		"Dodge",			"DodgeLogo",
-		"Ferrari",			"FerrariLogo",
-		"Jaguar", 			"JaguarLogo",
-		"Lotus", 			"LotusLogo",
-		"Porsche", 			"PorscheLogo",
+		"AlfaRomeo",	"AstonMartin",
+		"Dodge",		"Ferrari",
+		"Jaguar",		"Lotus",
+		"Porsche",
 	];
+	private _logoNameArray:string[] = [
+		"AlfaRomeoLogo",	"AstonMartinLogo",
+		"DodgeLogo",		"FerrariLogo",
+		"JaguarLogo",		"LotusLogo",
+		"PorscheLogo"
+	];
+	private _logoWindow:LogoWindow;
 
 	constructor() {
 		super();
@@ -29,24 +34,33 @@ export default class MainContainer extends Container {
 
 	private initImages():void {
         const loader:Loader = new Loader();
-		loader.add(this._pictureNameArray[0], "robot-1.jpg");
-		loader.add(this._pictureNameArray[1], "robot-2.jpg");
-		loader.add(this._pictureNameArray[2], "robot-3.jpg");
-		loader.add(this._pictureNameArray[3], "robot-4.jpg");
-		loader.add(this._pictureNameArray[4], "robot-5.jpg");
-		loader.add(this._pictureNameArray[5], "robot-6.jpg");
-		loader.add(this._pictureNameArray[6], "robot-7.jpg");
-		loader.add(this._pictureNameArray[7], "robot-8.jpg");
-		loader.add(this._pictureNameArray[8], "robot-9.jpg");
-		loader.add(this._pictureNameArray[9], "robot-9.jpg");
-		loader.add(this._pictureNameArray[10], "robot-9.jpg");
-		loader.add(this._pictureNameArray[11], "robot-9.jpg");
-		loader.add(this._pictureNameArray[12], "robot-9.jpg");
-		loader.add(this._pictureNameArray[13], "robot-9.jpg");
-		loader.load((loader, resources)=> {
+		loader.add(this._pictureNameArray[0], "AlfaRomeo.jpg");
+		loader.add(this._pictureNameArray[1], "AstonMartin.jpg");
+		loader.add(this._pictureNameArray[2], "Dodge.jpg");
+		loader.add(this._pictureNameArray[3], "Ferrari.jpg");
+		loader.add(this._pictureNameArray[4], "Jaguar.jpg");
+		loader.add(this._pictureNameArray[5], "Lotus.jpg");
+		loader.add(this._pictureNameArray[6], "Porsche.jpg");
 
-			console.log("loader");
+		loader.add(this._logoNameArray[0], "AlfaRomeoLogo.png");
+		loader.add(this._logoNameArray[1], "AstonMartinLogo.png");
+		loader.add(this._logoNameArray[2], "DodgeLogo.png");
+		loader.add(this._logoNameArray[3], "FerrariLogo.png");
+		loader.add(this._logoNameArray[4], "JaguarLogo.png");
+		loader.add(this._logoNameArray[5], "LotusLogo.png");
+		loader.add(this._logoNameArray[6], "PorscheLogo.png");
+
+		loader.load((loader, resources)=> {
+			this.initLogoWindow();
 		});
 		loader.load();
+	}
+
+	private initLogoWindow():void {
+		const gap:number = 10;
+		this._logoWindow = new LogoWindow(this._logoNameArray);
+		this.addChild(this._logoWindow);
+		this._logoWindow.x = (MainContainer.WIDTH - this._logoWindow.width) / 2;
+		this._logoWindow.y = MainContainer.HEIGHT - this._logoWindow.height - gap; 
 	}
 }
