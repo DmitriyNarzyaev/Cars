46e2642dcba51b4b3e2f1c888dd2ce409e2ba987
Dmitriy Narzyaev <nozaki@yandex.ru>
2022-03-23 17:04:57 +0300

    minor commit

--- src/LogoWindow.ts
+++ src/LogoWindow.ts
@@ -21,7 +21,7 @@ export default class LogoWindow extends Container {
 	}
 
 	private initLogo(logoNameArray:string[]):void {
-		const gap:number = 10;
+		const gap:number = 5;
 		let logoBackgroundX:number = 0;
 		let logoContainer:PIXI.Container = new PIXI.Container;
 		this.addChild(logoContainer);
@@ -29,7 +29,7 @@ export default class LogoWindow extends Container {
 		for (let iterator:number = 0; iterator < logoNameArray.length; iterator++) {
 			let logoBackground:PIXI.Graphics = new PIXI.Graphics;
 			logoBackground
-				.beginFill(0xaa4400)
+				.beginFill(0xbbbbbb)
 				.drawRect(0, 0, this._windowHeight, this._windowHeight);
 			logoBackground.x = logoBackgroundX;
 			logoBackgroundX += logoBackground.width + gap;
@@ -42,21 +42,26 @@ export default class LogoWindow extends Container {
 			logo.y = logoBackground.y + (logoBackground.height - logo.height) /2;
 			logoContainer.addChild(logo);
 		}
+		this.initBackground(logoContainer);
+		this.initMask(logoContainer);
+	}
 
+	private initBackground(logoContainer:PIXI.Container):void {
 		let logoContainerBackground:PIXI.Graphics = new PIXI.Graphics;
 		logoContainerBackground
-			.lineStyle(1, 0x000000, 0, 0)
+			.lineStyle(1, 0x000000)
 			.beginFill(0x000000, 1)
 			.drawRect(0, 0, logoContainer.width, logoContainer.height);
 		logoContainer.addChildAt(logoContainerBackground, 0);
+	}
 
+	private initMask(logoContainer:PIXI.Container):void {
 		let windowMask:PIXI.Graphics = new PIXI.Graphics;
 		windowMask
 			.lineStyle(1, 0x000000, 0, 0)
 			.beginFill(0x000000, 1)
 			.drawRect(0, 0, this._windowWidth, this._windowHeight);
 		this.addChild(windowMask);
-
 		logoContainer.mask = windowMask;
 	}
 }
